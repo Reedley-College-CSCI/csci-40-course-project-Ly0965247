235e2afbafe8cb83b1600404680adf9f2884e3f8
Lucas Yang <0965247@outlook.com>
2025-11-15 18:33:27 -0800

    Added the real description of the program and its purpose

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 /* 
  * Description:
- *
+ * This program will allow the user to be able to create a Dungeons and Dragons
+ * character and keep track of notes for quests, equipment, level, and stats.
 */
 
 #include <iostream>
